c35d5357d587f5b874dc99d11c6346c915669c15
beetlejusse <vishalbelwal52@gmail.com>
2024-12-28 14:11:44 +0530

    navbar modifications

--- components/customs/NavBar.tsx
+++ components/customs/NavBar.tsx
@@ -1,26 +1,47 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import { Button } from "../ui/button";
 import { useRouter } from "next/navigation";
 
 const Navbar: React.FC = () => {
+  const [isBlurred, setIsBlurred] = useState(false); // Default state without blur
   const githubAvatarUrl = process.env.NEXT_PUBLIC_GITHUBUSERNAME
     ? `https://github.com/${process.env.NEXT_PUBLIC_GITHUBUSERNAME}.png`
-    : "/default-avatar.png"; 
+    : "/default-avatar.png";
 
-  const router = useRouter(); 
+  const router = useRouter();
 
   const handleGetInTouchClick = () => {
     router.push("/touch");
   };
 
+  const handleJourneyClick = () => {
+    router.push("/journey");
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      setIsBlurred(scrollY > 20); // Apply blur effect when user scrolls 20px or more
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <nav
       className={cn(
-        "fixed top-0 left-0 w-full flex items-center justify-between py-4 px-6 bg-black text-white z-50"
+        "fixed top-0 left-0 w-full flex items-center justify-between py-4 px-6 z-50 transition-all duration-300",
+        isBlurred
+          ? "bg-black/50 backdrop-blur-md text-white shadow-lg"
+          : "bg-black text-white"
       )}
     >
       <div className="flex items-center space-x-3">
@@ -31,10 +52,15 @@ const Navbar: React.FC = () => {
           alt="GitHub Profile"
           className="rounded-full object-cover cursor-pointer"
           onClick={() => {
-            router.push("/")
+            router.push("/");
           }}
         />
-        <span className="font-bold text-zinc-300 cursor-pointer">JOURNEY</span>
+        <span
+          className="font-bold text-zinc-300 cursor-pointer"
+          onClick={handleJourneyClick}
+        >
+          JOURNEY
+        </span>
       </div>
 
       <div className="flex items-center space-x-4">
